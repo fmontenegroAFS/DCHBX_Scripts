56574ae55be5fb5abe98ea8e81874465f8409807
fmontenegroAFS <francis.montenegro@accenturefederal.com>
2015-12-01 16:09:59 -0500

    Changes for very large data set

--- IntegratedIndividualUnassisted/GenerateFamilyMemberSSN.c
+++
@@ -1,9 +0,0 @@
-GenerateFamilyMemberSSN()
-{
-	
-	char userSSN[32];
-	sprintf(userSSN,"%s-%s-%s",lr_eval_string("{UserSSNFirst3}"),lr_eval_string("{UserSSNSecond2}"),lr_eval_string("{UserSSNLast4}"));
-	lr_save_string(userSSN,"familyMemberSSN");
-	
-	return 0;
-}
